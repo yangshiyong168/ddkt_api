127cbd6664a42439f0b1feb4b940a8e0d2f2d4d2
yangshiyong168 <yangshiyong668@163.com>
2020-11-24 20:35:45 +0800

    test

--- conftest.py
+++ conftest.py
@@ -1,10 +1,7 @@
 import time,os
 import pytest,allure
-
-# def pytest_sessionstart(session):
-#     session.results = dict()
 print("haihaixing")
 print("haha")
 print("nihao)
 print("nihao)
-广西
+广西桂平
