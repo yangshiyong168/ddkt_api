13cba2b365acb6264e931ec06d28000fb7193d48
yangshiyong168 <yangshiyong668@163.com>
2020-12-26 17:55:56 +0800

    django

---
+++ djangoproject/testproject/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+# Register your models here.
+# admin.site.register() 后面添加的
---
+++ djangoproject/testproject/views.py
@@ -0,0 +1,43 @@
+﻿
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+# from django.contrib.auth import authenticate, login
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+import pymysql
+
+
+
+def welcome(request):
+    return render(request,"welcome.html")
+
+def home(request):
+    return render(request,"home.html")
+
+def left(request):
+    return render(request,"left.html")
+
+def login(request):
+    if request.POST:
+        username = password = ''
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            request.session['user'] = username 
+            response = HttpResponseRedirect('/home/')
+            return response
+        else:
+            return render(request,'login.html', {'error': '用户名或密码错误'})
+    return render(request,'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request,'login.html')
+
+
+
